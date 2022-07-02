d9265ecaeab9a7f3a920aafc4171ee20eb8ada42
NakagawaRyunosuke <s2032112gn@s.chibakoudai.jp>
2022-07-02 16:48:16 +0900

    edit

--- index.js
+++ index.js
@@ -34,11 +34,72 @@ function init(){
     ground.position.set(0,0,0);
     scene.add(ground);
 
+    // グループ
+    const group = new THREE.Group();
+    scene.add( group );
+    // パーティクル
+    const num = 1000; // パーティクルの数
+    const range = 1000; // 配置する範囲
+    //const rangeHalf = range / 2;
+
+    const material = new THREE.SpriteMaterial({color: 0xffffff});
+    for ( let i = 0; i < num; i ++ ) {
+        const sprite = new THREE.Sprite( material ); //常に正面を向く3Dオブジェクト
+        sprite.position.x = range * (Math.random() - 0.5);
+        sprite.position.y = range * (Math.random() - 0.5);
+        sprite.position.z = range * (Math.random() - 0.5);
+        sprite.scale.x = sprite.scale.y = sprite.scale.z = Math.random() * 10 + 5;
+        sprite.matrixAutoUpdate = false;
+        sprite.updateMatrix();
+        group.add( sprite );
+    }
+
+    //位置を変える------------------
+    const windX = 0.25; //x方向の速度（共通）
+    const variationX = 0.25; //x方向の速度にバラつきを加える
+    const gravityY = 0.5 //y方向の速度（共通）
+    const variationY = 0.25;//y方向の速度にバラつきを加える
+    const windZ = 0; //z方向の速度（共通）
+    const variationZ = 0.5;//z方向の速度にバラつきを加える
+
+    const positionUpdate = () =>{
+
+        for ( let i = 0; i < num; i ++ ) {
+
+            const obj = group.children[ i ];
+            //x
+            if( obj.position.x < - rangeHalf){
+            obj.position.x = rangeHalf;
+            }else if ( obj.position.x > rangeHalf){
+            obj.position.x = - rangeHalf;
+            }else{
+            obj.position.x +=  windX + variationX * ( i / num - 0.5  ) ;
+            }
+            //y
+            if( obj.position.y < - rangeHalf){
+            obj.position.y = rangeHalf;
+            }else{
+            obj.position.y -= gravityY + variationY * i / num ;
+            }
+            //z
+            if( obj.position.z < - rangeHalf){
+            obj.position.z = rangeHalf;
+            }else if ( obj.position.z > rangeHalf){
+            obj.position.z = - rangeHalf;
+            }else{
+            obj.position.z +=  windZ + variationZ * ( i / num - 0.5  ) ;
+            }
+            obj.matrixAutoUpdate = false;
+            obj.updateMatrix();
+        }
+
+    }
+
     tick();
 
     function tick(){
         renderer.render(scene, camera); // レンダリング
-
+        positionUpdate();
         requestAnimationFrame(tick);
     }
 }
