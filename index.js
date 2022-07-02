1f7077da28bf7c8a634202e98a362a00bc59c7bf
NakagawaRyunosuke <s2032112gn@s.chibakoudai.jp>
2022-07-02 15:55:49 +0900

    edit

--- index.js
+++ index.js
@@ -12,21 +12,21 @@ function init(){
 
     // シーンを作成
     const scene = new THREE.Scene();
-    //scene.fog = new THREE.Fog( 0xEEEEEE, 0, 100 );
+    scene.fog = new THREE.Fog( 0xeeeeee, 0, 100 );
 
     // カメラを作成
     const camera = new THREE.PerspectiveCamera(45, width / height);
-    camera.position.set(0, 0, 1000);
+    camera.position.set(0, 0, 100);
     // カメラコントローラーを作成
     const controls = new THREE.OrbitControls(camera, document.body);
 
-    //床
-    const groundGeometry = new THREE.PlaneGeometry(150,150,64,64);
-    const snow = new THREE.TextureLoader().load("img/snow.jpg");
-    const groundMaterial = new THREE.MeshLambertMaterial({map:snow});
-    const ground = new THREE.Mesh(groundGeometry, groundMaterial);
-    ground.rotation.x = Math.PI / -2;
-    scene.add(ground);
+    // //床
+    // const groundGeometry = new THREE.PlaneGeometry(150,150,64,64);
+    // const snow = new THREE.TextureLoader().load("img/snow.jpg");
+    // const groundMaterial = new THREE.MeshLambertMaterial({map:snow});
+    // const ground = new THREE.Mesh(groundGeometry, groundMaterial);
+    // ground.rotation.x = Math.PI / -2;
+    // scene.add(ground);
 
 
     tick();
