cb752ad28b6a0ee4a56f449db4339df01b43bcc6
NakagawaRyunosuke <s2032112gn@s.chibakoudai.jp>
2022-07-03 19:40:12 +0900

    edit

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function init(){
 
     // シーンを作成
     const scene = new THREE.Scene();
-    scene.fog = new THREE.Fog(0x333333, 100, 1000);
+    scene.fog = new THREE.Fog(0x333333, 10, 100);
 
     // 平行光源を作成
     // new THREE.DirectionalLight(色, 光の強さ)
