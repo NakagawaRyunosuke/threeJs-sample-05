4172ef217c7cbeadd4db752337478feb3be79932
NakagawaRyunosuke <s2032112gn@s.chibakoudai.jp>
2022-07-03 16:14:55 +0900

    edit

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function init(){
 
     // カメラを作成
     const camera = new THREE.PerspectiveCamera(45, width / height);
-    camera.position.set(0, 4, 10);
+    camera.position.set(0, 10, 10);
     // カメラコントローラーを作成
     const controls = new THREE.OrbitControls(camera, document.getElementById("canvas"));
 
