13ffe5e248af5b93999d14e56a2fa3bd2207e931
NakagawaRyunosuke <s2032112gn@s.chibakoudai.jp>
2022-07-03 17:14:33 +0900

    edit

--- index.js
+++ index.js
@@ -100,16 +100,16 @@ function init(){
     }
 
     //雪だるま
-    const BGeometry = new THREE.SphereGeometry( 10, 32, 32 );
+    const BGeometry = new THREE.SphereGeometry( 100, 32, 32 );
     const BMaterial = new THREE.MeshLambertMaterial( {color: 0xffffff,map:snow} );
     const snowman_B = new THREE.Mesh( BGeometry, BMaterial );
-    snowman_B.position.set(0,10,0);
+    snowman_B.position.set(0,100,0);
     scene.add( snowman_B );
 
-    const TGeometry = new THREE.SphereGeometry( 5, 32, 32 );
+    const TGeometry = new THREE.SphereGeometry( 50, 32, 32 );
     const TMaterial = new THREE.MeshLambertMaterial( {color: 0xffffff,map:snow} );
     const snowman_T = new THREE.Mesh( TGeometry, TMaterial );
-    snowman_T.position.set(0,100,0);
+    snowman_T.position.set(0,150,0);
     scene.add(snowman_T);
 
     tick();
