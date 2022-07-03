4e464228be68d98bfb785578424cd4f9886a2e65
NakagawaRyunosuke <s2032112gn@s.chibakoudai.jp>
2022-07-03 17:20:13 +0900

    edit

--- index.js
+++ index.js
@@ -111,9 +111,8 @@ function init(){
     snowmanGroup.add( snowman_B );
 
     const TGeometry = new THREE.SphereGeometry( 50, 32, 32 );
-    const TMaterial = new THREE.MeshLambertMaterial( {color: 0xffffff,map:snow} );
-    const snowman_T = new THREE.Mesh( TGeometry, TMaterial );
-    snowman_T.position.set(0,150,0);
+    const TMaterial = new THREE.MeshLambertMaterial({color:0xffffff, map:snow});
+    const snowman_T = new THREE.Mesh( TGeometry, TMaterial);
     snowmanGroup.add(snowman_T);
 
     tick();
